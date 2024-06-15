c49296c618531abc169e6148e31b30ed3a7cf253
Ludwig Céspedes Ogando <ludwigcespedesogando@gmail.com>
2024-06-14 23:11:21 -0400

    Si

---
+++ game/menu.py
@@ -0,0 +1,9 @@
+from board import *
+class Menu:
+    def __init__(self,players = 2,):
+        self.size = 0
+        self.board = Board(self.size)
+        self.players = players
+
+    
+    
--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from game.piece import *
 def main():
     pass
 def main1():
-    game=Board(5)
+    game=Board(20)
     jugador = Player("Luis","yellow")
     game.place_piece(jugador.P_17,[0,0])
+    game.place_piece(jugador.P_10,[15,12])
     game.display_board()
 
 if __name__ == "__main__":
